8411905103627c327aaf1f8f700db3b8450664e2
Juan Pablo Buritica <juanpablo@buritica.org>
2011-04-11 14:42:52 -0400

    added get clicky

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -27,6 +27,17 @@ if(!defined('BASEPATH')) exit('No direct script access allowed');
 <script src="<?php echo base_url() ?>js/script.js"></script>
 <!-- end scripts-->
 
+<script type="text/javascript">
+var clicky_site_id = 66410187;
+(function() {
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = '//static.getclicky.com/js';
+  ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+})();
+</script>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/66410187ns.gif" /></p></noscript>
 </body>
 </html>
 <?php  
