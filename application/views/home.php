5b6f984f8ece8302ba0eb3c15b9b946d703faad4
Juan Pablo Buritica <juanpablo@buritica.org>
2011-09-20 05:32:06 -0400

    Started with bios
    - Tom Preston
    - Paul Irish

--- application/views/home.php
+++ application/views/home.php
@@ -7,12 +7,64 @@
 </div>
 <nav class="main-nav">
 	<div class="wrapper">
-		<a href="#conferencistas" class="href active">Conferencistas</a>
+		<!-- <a href="#conferencistas" class="href active">Conferencistas</a> -->
 	</div>
 </nav>
 <div id="dark-green">
 	<div class="wrapper">
-
+		<div id="speakers" class="content-left">
+			<h2 id="title">Conferencistas</h2>
+			<div class="speaker">
+				<div class="avatar tpreston">
+					<a href="http://twitter.com/#!/mojombo" target="blank" class="twitter"></a>
+				</div>
+				<div class="bio">
+					<h3 class="name">Tom Preston-Werner</h3>
+					<h5 class="talk">Charla - Keynote de Cierre</h5>
+					<p>Cofundador y CTO de <a href="http://github.com" target="blank">GitHub</a> y considerado como uno de los emprendedores  m&aacute;s exitosos de 2009. Es lider en la comunidad de Ruby y fund&oacute; y administr&oacute; <a href="http://gravatar.com" target="blank">Gravatar</a> antes de fundar GitHub</p>
+				</div>				
+			</div>
+			<div class="speaker">
+				<div class="avatar pirish">
+					<a href="http://twitter.com/#!/paul_irish" target="blank" class="twitter"></a>
+				</div>
+				<div class="bio">
+					<h3 class="name">Paul Irish</h3>
+					<!-- <h5 class="talk"></h5> -->
+					<p>Front-end Developer y diseñador de UX. Es parte del equipo de Relaciones de Google Chrome, al igual que el de jQuery. Miembro de <a href="http://yayquery.com/" target="_blank">yayQuery</a>, un Podcast sobre JavaScript.</p>
+					<p>Mantiene activamente varios proyectos Open Source, tales como <a href="http://modernizr.com" target="blank">modernizr</a> y <a href="http://h5bp.com" target="_blank">html5boilerplate.</a></p>
+				</div>				
+			</div>
+			<div class="speaker">
+				<div class="avatar tpreston">
+					<a href="http://twitter.com/#!/mojombo" target="blank" class="twitter"></a>
+				</div>
+				<div class="bio">
+					<h3 class="name">Tom Preston-Werner</h3>
+					<h5 class="talk">Charla - Keynote de Cierre</h5>
+					<p>Cofundador y CTO de <a href="http://github.com" target="blank">GitHub</a> y considerado como uno de los emprendedores  m&aacute;s exitosos de 2009. Es lider en la comunidad de Ruby y fund&oacute; y administr&oacute; <a href="http://gravatar.com" target="blank">Gravatar</a> antes de fundar GitHub</p>
+				</div>				
+			</div>
+		</div>
+		<div id="sponsors" class="side-bar">
+			<h4>&iquest;C&uacute;ando?</h4>
+			<p class="desc">1ro de Noviembre, 2011. Bogotá, Colombia.</p>
+			<h4>&iquest;C&uacute;anto cuesta?</h4>
+			<p>Estudiantes - 50 cupos</p>
+			<ul class="cost">
+				<li>$70,000 antes del 10 de Oct.</li>
+				<li>$90,000 hasta el 30 de Oct.</li>
+				<li>Entradas de estudiante a la venta pronto con carnet vigente.</li>
+			</ul>
+			<p>Profesionales - 100 cupos</p>
+			<ul class="cost">
+				<li>$130,000 antes del 10 de Oct</li>
+				<li>$150,000 hasta el 30 de Oct.</li>
+				<li>Entradas profesionales a la venta en <a href="http://bogotaconf.eventbrite.com" target="_blank">Eventbrite</a></li>
+			</ul>
+			<h4>&iquest;Donde?</h4>
+			<p>Por confirmar</p>
+		</div>
 	</div>
 </div>
 
