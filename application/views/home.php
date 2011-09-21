13acb01e2df21ca24a805ee0d2824751b422ac2b
Giovanny <giovanny@Pavilion-dv3500.(none)>
2011-09-20 23:58:52 -0500

    Se agrega la bio de los conferencistas y panelistas del evento.

--- application/views/home.php
+++ application/views/home.php
@@ -36,98 +36,95 @@
 				</div>				
 			</div>
 			<div class="speaker">
-				<div class="avatar tpreston">
-					<a href="http://twitter.com/#!/mojombo" target="blank" class="twitter"></a>
+				<div class="avatar ?">
+					<a href="http://twitter.com/#!/?" target="blank" class="twitter"></a>
 				</div>
 				<div class="bio">
-					<h3 class="name">Tom Preston-Werner</h3>
-					<h5 class="talk">Charla - Keynote de Cierre</h5>
-					<p>Cofundador y CTO de <a href="http://github.com" target="blank">GitHub</a> y considerado como uno de los emprendedores  m&aacute;s exitosos de 2009. Es lider en la comunidad de Ruby y fund&oacute; y administr&oacute; <a href="http://gravatar.com" target="blank">Gravatar</a> antes de fundar GitHub</p>
+					<h3 class="name">Obie Fernandez</h3>
+					<p>Autor del libro '<a href="http://www.amazon.com/Rails-Way-Addison-Wesley-Professional-Ruby/dp/0321601661" target="blank">The 				Rails Way, </a>the definitive reference guide for Ruby on Rails', de Addison-Wesley Professional Editorial. <p>Fundador de <a href="http://hashrocket.com/" target="blank">Hashrocket</a>, y miembro reconocido en la comunidad internacional de Ruby</p>
 				</div>				
 			</div>
 			<div class="speaker">
-				<div class="avatar pirish">
-					<a href="http://twitter.com/#!/paul_irish" target="blank" class="twitter"></a>
+				<div class="avatar ?">
+					<a href="http://twitter.com/#!/?" target="blank" class="twitter"></a>
 				</div>
 				<div class="bio">
-					<h3 class="name">Paul Irish</h3>
+					<h3 class="name">Edgar Gonzalez</h3>
 					<!-- <h5 class="talk"></h5> -->
-					<p>Front-end Developer y diseñador de UX. Es parte del equipo de Relaciones de Google Chrome, al igual que el de jQuery. Miembro de <a href="http://yayquery.com/" target="_blank">yayQuery</a>, un Podcast sobre JavaScript.</p>
-					<p>Mantiene activamente varios proyectos Open Source, tales como <a href="http://modernizr.com" target="blank">modernizr</a> y <a href="http://h5bp.com" target="_blank">html5boilerplate.</a></p>
+					<p>Agile web developer y project manager, Experto en Ruby on Rails, Cto de <a href="http://piictu.com/" target="blank">Piictu</a>, Tambi&eacute; es cofundador de otras iniciativas como <a href="http://www.HasManyDevelopers.com" target="blank">HasManyDevelopers.com</a> y <a href="http://rubycorner.com" target="blank">rubycorner.com</a></p>
+					<p>Es profesor de Ciencias de la computaci&oacute;n en la Universidad Central de Venezuela</p>
 				</div>				
 			</div>
 			<div class="speaker">
-				<div class="avatar tpreston">
-					<a href="http://twitter.com/#!/mojombo" target="blank" class="twitter"></a>
+				<div class="avatar ?">
+					<a href="http://twitter.com/#!/?" target="blank" class="twitter"></a>
 				</div>
 				<div class="bio">
-					<h3 class="name">Tom Preston-Werner</h3>
-					<h5 class="talk">Charla - Keynote de Cierre</h5>
-					<p>Cofundador y CTO de <a href="http://github.com" target="blank">GitHub</a> y considerado como uno de los emprendedores  m&aacute;s exitosos de 2009. Es lider en la comunidad de Ruby y fund&oacute; y administr&oacute; <a href="http://gravatar.com" target="blank">Gravatar</a> antes de fundar GitHub</p>
+					<h3 class="name">Sebastian Delmont</h3>
+					<p>Cofundador y CTO de <a href="http://StreetEasy.com" target="blank">StreetEasy.com</a> en New York. Sebastian es bastante activo en las comunidades de desarrollo tanto de Ruby como de Android. Él tiene mas de 15 a&nacute;os de experiencia en ingenier&iacute;a de software.</p>
+<p>También esta involucrado en el desarrollo de una versi&oacute;n de android para Gilt, <a href="http://hootsuite.com/" target="blank">Hootsuite</a> y <a href="http://groupme.com/" target="blank">GroupMe</a></p>
+
 				</div>				
 			</div>
 			<div class="speaker">
-				<div class="avatar pirish">
-					<a href="http://twitter.com/#!/paul_irish" target="blank" class="twitter"></a>
+				<div class="avatar ?">
+					<a href="http://twitter.com/#!/?" target="blank" class="twitter"></a>
 				</div>
 				<div class="bio">
-					<h3 class="name">Paul Irish</h3>
-					<!-- <h5 class="talk"></h5> -->
-					<p>Front-end Developer y diseñador de UX. Es parte del equipo de Relaciones de Google Chrome, al igual que el de jQuery. Miembro de <a href="http://yayquery.com/" target="_blank">yayQuery</a>, un Podcast sobre JavaScript.</p>
-					<p>Mantiene activamente varios proyectos Open Source, tales como <a href="http://modernizr.com" target="blank">modernizr</a> y <a href="http://h5bp.com" target="_blank">html5boilerplate.</a></p>
+					<h3 class="name">Harold Gimenez</h3>
+					<p>Desarrollador Web para <a href="http://thoughtbot.com/" target="blank">Thoughtbot</a> en Boston. Es un miembro bastante activo en la comunidad Ruby on Rails.</p>
+<p> Actualmente est&aacute; trabajando en el desarrollo de <a href="http://bestdamnburger.com/" target="blank">bestdamnburger.com</a>
 				</div>				
 			</div>
 			<h2 class="title">Panelistas</h2>
 			<div class="speaker">
-				<div class="avatar tpreston">
-					<a href="http://twitter.com/#!/mojombo" target="blank" class="twitter"></a>
+				<div class="avatar ?">
+					<a href="http://twitter.com/#!/?" target="blank" class="twitter"></a>
 				</div>
 				<div class="bio">
-					<h3 class="name">Tom Preston-Werner</h3>
-					<h5 class="talk">Charla - Keynote de Cierre</h5>
-					<p>Cofundador y CTO de <a href="http://github.com" target="blank">GitHub</a> y considerado como uno de los emprendedores  m&aacute;s exitosos de 2009. Es lider en la comunidad de Ruby y fund&oacute; y administr&oacute; <a href="http://gravatar.com" target="blank">Gravatar</a> antes de fundar GitHub</p>
+
+					<h3 class="name">Alex Torrenegra</h3>
+
+					<p>Emprendedor Colombiano residente en New York. Es el CEO y Co-fundador de <a href="http://torrenegralabs.com" target="blank">TorrenegraLabs</a></p>
 				</div>				
 			</div>
 			<div class="speaker">
-				<div class="avatar pirish">
-					<a href="http://twitter.com/#!/paul_irish" target="blank" class="twitter"></a>
+				<div class="avatar ?">
+					<a href="http://twitter.com/#!/?" target="blank" class="twitter"></a>
 				</div>
 				<div class="bio">
-					<h3 class="name">Paul Irish</h3>
-					<!-- <h5 class="talk"></h5> -->
-					<p>Front-end Developer y diseñador de UX. Es parte del equipo de Relaciones de Google Chrome, al igual que el de jQuery. Miembro de <a href="http://yayquery.com/" target="_blank">yayQuery</a>, un Podcast sobre JavaScript.</p>
-					<p>Mantiene activamente varios proyectos Open Source, tales como <a href="http://modernizr.com" target="blank">modernizr</a> y <a href="http://h5bp.com" target="_blank">html5boilerplate.</a></p>
+					<h3 class="name">Andres Barreto</h3>
+					<p>Emprendedor Colombiando residente en New York. Cofundador de <a href="http://grooveshark.com/" target="blank">Grooveshark</a> y <a href="http://onswipe.com/" target="blank">onSwipe</a>.</p>
 				</div>				
 			</div>
 			<div class="speaker">
-				<div class="avatar tpreston">
-					<a href="http://twitter.com/#!/mojombo" target="blank" class="twitter"></a>
+				<div class="avatar ?">
+					<a href="http://twitter.com/#!/?" target="blank" class="twitter"></a>
 				</div>
 				<div class="bio">
-					<h3 class="name">Tom Preston-Werner</h3>
-					<h5 class="talk">Charla - Keynote de Cierre</h5>
-					<p>Cofundador y CTO de <a href="http://github.com" target="blank">GitHub</a> y considerado como uno de los emprendedores  m&aacute;s exitosos de 2009. Es lider en la comunidad de Ruby y fund&oacute; y administr&oacute; <a href="http://gravatar.com" target="blank">Gravatar</a> antes de fundar GitHub</p>
+					<h3 class="name">Jonathan Tarud</h3>
+					<p>Emprendedor Colombiano residente en Barranquilla. Fundador de <a href="http://koombea.com" target="blank">Koombea</a></p>
 				</div>				
 			</div>
 			<div class="speaker">
-				<div class="avatar pirish">
-					<a href="http://twitter.com/#!/paul_irish" target="blank" class="twitter"></a>
+				<div class="avatar ?">
+					<a href="http://twitter.com/#!/?" target="blank" class="twitter"></a>
 				</div>
 				<div class="bio">
-					<h3 class="name">Paul Irish</h3>
-					<!-- <h5 class="talk"></h5> -->
-					<p>Front-end Developer y diseñador de UX. Es parte del equipo de Relaciones de Google Chrome, al igual que el de jQuery. Miembro de <a href="http://yayquery.com/" target="_blank">yayQuery</a>, un Podcast sobre JavaScript.</p>
-					<p>Mantiene activamente varios proyectos Open Source, tales como <a href="http://modernizr.com" target="blank">modernizr</a> y <a href="http://h5bp.com" target="_blank">html5boilerplate.</a></p>
+					<h3 class="name">Alan Colmenares</h3>
+					<p>Emprendedore e inversionista Norteamericano residente en Bogot&aacute;. Fundador del <a href="http://www.founderinstitute.com/apply/bogota" target="blank">Founder Institute</a> en Colombia</p>
 				</div>				
 			</div>
 			<div class="speaker">
-				<div class="avatar tpreston">
-					<a href="http://twitter.com/#!/mojombo" target="blank" class="twitter"></a>
+				<div class="avatar ?">
+					<a href="http://twitter.com/#!/?" target="blank" class="twitter"></a>
 				</div>
 				<div class="bio">
-					<h3 class="name">Tom Preston-Werner</h3>
-					<h5 class="talk">Charla - Keynote de Cierre</h5>
-					<p>Cofundador y CTO de <a href="http://github.com" target="blank">GitHub</a> y considerado como uno de los emprendedores  m&aacute;s exitosos de 2009. Es lider en la comunidad de Ruby y fund&oacute; y administr&oacute; <a href="http://gravatar.com" target="blank">Gravatar</a> antes de fundar GitHub</p>
+					<h3 class="name">Juan Pablo Buritic&aacute;</h3>
+
+
+					<p>Emprendedor Colombiano e ingeniero de software residente en New York. Cofundador e Ingeniero Jefe en <a href="http://panelfly.com" target="blank">Panelfly</a>.</p>
+					<p>Fundador de la iniciativa BogotaConf.</p>
 				</div>				
 			</div>
 		</div>
